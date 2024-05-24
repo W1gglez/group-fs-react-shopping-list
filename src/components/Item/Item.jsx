f75ad29e5923e1cb3b80a1cfaa4756fa1c190ad8
Logan Weigel <snr.wiggels@gmail.com>
2024-05-24 13:21:48 -0500

    Modify easter egg button

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -11,46 +11,47 @@ import 'animate.css';
 export default function Item({ item, fetchInventory }) {
   function removeItem(id) {
     Swal.fire({
-      title: "Are you sure?",
+      title: 'Are you sure?',
       text: "You won't be able to revert this!",
-      icon: "warning",
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
       showClass: {
         popup: `
           animate__animated
           animate__fadeInUp
           animate__faster
-        `
+        `,
       },
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire({
-          title : "Deleted!",
-          text: "Your file has been deleted.",
-          icon: "success",
-          imageUrl: "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnVra2xkbGxyZ2RpeWYyY2F0ZnpsNzBoNms3MHBiaTkwd3p5Z2hxNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/k3YNVBrbn2KqbXEgDJ/giphy.gif",
-  imageWidth: 400,
-  imageHeight: 200,
-  imageAlt: "Custom image",
+          title: 'Deleted!',
+          text: 'Your file has been deleted.',
+          icon: 'success',
+          imageUrl:
+            'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnVra2xkbGxyZ2RpeWYyY2F0ZnpsNzBoNms3MHBiaTkwd3p5Z2hxNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/k3YNVBrbn2KqbXEgDJ/giphy.gif',
+          imageWidth: 400,
+          imageHeight: 200,
+          imageAlt: 'Custom image',
           hideClass: {
             popup: `
               animate__animated
               animate__tada
               animate__flip
               animate__faster
-            `
-          }
+            `,
+          },
         });
         axios
           .delete(`/api/inventory/${id}`)
           .then(() => fetchInventory())
           .catch((err) => console.error(err));
       }
-      }
-    )};
+    });
+  }
 
   function purchaseItem(id) {
     console.log(id);
@@ -59,13 +60,12 @@ export default function Item({ item, fetchInventory }) {
       .then(() => fetchInventory())
       .catch((err) => console.error(err));
   }
-function sunModal() {
-  Swal.fire({
-    imageUrl: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjdpNzcxNDRrZjB2eDJzdjBnZ3c2Z3JuNzVjdmZleW1wdXE0N2pvayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/10ltVBrN9bO5d6/giphy.webp",
-  
-  });
-}
-
+  function sunModal() {
+    Swal.fire({
+      imageUrl:
+        'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjdpNzcxNDRrZjB2eDJzdjBnZ3c2Z3JuNzVjdmZleW1wdXE0N2pvayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/10ltVBrN9bO5d6/giphy.webp',
+    });
+  }
 
   return (
     <Col>
@@ -74,7 +74,17 @@ function sunModal() {
         style={{ width: '15rem' }}
       >
         <Card.Body>
-          <Card.Title>{item.name}</Card.Title>
+          <Card.Title>
+            {item.name}{' '}
+            {item.name.includes('sun') ? (
+              <Button
+                variant='light'
+                onClick={() => sunModal()}
+              >
+                <strong>🌞</strong>
+              </Button>
+            ) : null}
+          </Card.Title>
           <Card.Text>
             Quantity: {item.quantity} {item.unit}
             <br />
@@ -103,18 +113,14 @@ function sunModal() {
                 Remove
               </Button>
             }
-            {if(item.name.includes('s','u','n')){
-            <button onClick={()=> sunModal()}><strong>🌞</strong></button>
-            }}
           </Card.Text>
         </Card.Body>
       </Card>
     </Col>
-
   );
 }
 
 
-  }
+
 
 
