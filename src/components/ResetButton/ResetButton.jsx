41294a2441706e4d56a6b7fa56ef850b7ac9c455
Alex <adahl1993@gmail.com>
2024-05-24 11:34:21 -0500

    added warning message before delete and easter egg

--- src/components/ResetButton/ResetButton.jsx
+++ src/components/ResetButton/ResetButton.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 export default function ResetButton({ invList, fetchInventory }) {
   function reset() {
@@ -14,10 +15,48 @@ export default function ResetButton({ invList, fetchInventory }) {
   }
 
   function clearShoppingCart() {
-    axios
-      .delete(`/api/inventory`)
-      .then(() => fetchInventory())
-      .catch((err) => console.error(err));
+    Swal.fire({
+      title: "Are you sure?",
+      text: "This will remove EVERYTHING!!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+      showClass: {
+        popup: `
+          animate__animated
+          animate__fadeInUp
+          animate__faster
+        `
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "SUCCESS",
+          icon: "success",
+          imageUrl: "https://i.gifer.com/1Q49.gif",
+          imageWidth: 400,
+          imageHeight: 200,
+          imageAlt: "Custom image",
+          hideClass: {
+            popup: `
+              animate__animated
+              animate__fadeOutDown
+              animate__faster
+            `
+          }
+        });
+        axios
+          .delete(`/api/inventory`)
+          .then(() => fetchInventory())
+          .catch((err) => console.error(err));
+      }
+    });
+
+
+
   }
 
   return (
